6916df302021b33d0cafb85cbfe50836eb5bcc12
Jaqueline Da Aparecida Ribeiro <56163078+Jaqueline-jpg@users.noreply.github.com>
2021-12-01 21:43:35 -0300

    Envio

--- daw-loja/server/database/index.js
+++ daw-loja/server/database/index.js
@@ -1,10 +1,11 @@
 const knex = require('knex')({
-    client: 'mysql',
+    client: 'mysql2',
     connection: {
         host: 'localhost',
         user: 'root',
-        password: 'root',
-        database: 'dawdb'
+        password: 'jaqueline12@',
+        database: 'dawdb',
+        port: 333
     }
 });
 
